9db25b61bae676b73da0e6c8fdf53d7d21a4a2c9
VictorNGomes <50057809+VictorNGomes@users.noreply.github.com>
2019-04-27 17:32:45 -0300

    Create Etapa01.js

---
+++ Etapa01.js
@@ -0,0 +1,16 @@
+/* 
+    Equipe: 
+        Victor do Nascimento Gomes - Subturma A (Líder) 
+        Fellype Eduardo de Farias Martins -    Subturma A 
+        Etapa 1
+*/
+function setup() { 
+createCanvas(400, 400); 
+}
+
+function draw() { 
+background(210);
+
+ellipse(200, 350, 50, 50) 
+rect(170, 50, 60, 60) 
+}
