82e57463bb3566c54caf2ab98d5b3fccf5647b93
VictorNGomes <50057809+VictorNGomes@users.noreply.github.com>
2019-04-27 18:00:04 -0300

    Figura geométrica e movimentação do jogador

--- JogoEtapa1eEtapa2.js
+++ JogoEtapa1eEtapa2.js
@@ -1,6 +1,6 @@
 /* 
     Equipe: 
-        Victor do Nascimento Gomes - Subturma A (Líder) 
+        Victor do Nascimento Gomes - Subturma A  (Líder) 
         Fellype Eduardo de Farias Martins - Subturma A 
         Etapa 1 e 2
 */
